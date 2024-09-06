a8e3eb4c13b6f3d89980eabb496a9fe3a207692f
ANONYMOUS-JARVIS <muthurajpostbox@gmail.com>
2024-09-06 08:45:41 +0530

    vercel_updated

--- mobile/settings.py
+++ mobile/settings.py
@@ -125,6 +125,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
+
+
 STATIC_URL = 'static/'
 MEDIA_ROOT='/media/'
 STATICFILES_DIRS = [
@@ -135,6 +137,7 @@ STATICFILES_DIRS = [
 STATIC_ROOT = os.path.join(BASE_DIR,'staticfiles_build','static')
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
+
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 MEDIA_ROOT=os.path.join(BASE_DIR,'static/media')
